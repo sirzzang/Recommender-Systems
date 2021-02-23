7f23795fa423ca3b71bf8c2efd8a5e61a3302195
sirzzang <sirzzang@naver.com>
2021-02-23 19:28:23 +0900

    20210223 gitignore update

--- utils.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def load_data(path):
-    if path.split('.')[-1] == 'dat':
-        df = pd.read_csv(path, sep='::', header=None, engine='python')
-    else:
-        df = pd.read_csv(path)
-    return df
